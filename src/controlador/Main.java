7464db5481c6599b49d4d13b1dbec7d3c822a81d
Oscar Munoz <omamr0304@gmail.com>
2023-09-28 15:05:32 -0500

    Creacion ventana introduccion

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Prueba rama fernando");
+        VentanaIntroduccion v1 = new VentanaIntroduccion();
+        v1.setVisible(true);
+        
     }
     
 }
